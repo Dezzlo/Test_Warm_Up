8d67947d5f905917ef9f2543a7256d5995372acc
PryTonick <42908445+Dezzlo@users.noreply.github.com>
2018-09-22 12:54:24 +0300

    Update Warm_UpTest.java

--- test/java/Warm_UpTest.java
+++ test/java/Warm_UpTest.java
@@ -2,22 +2,36 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 public class Warm_UpTest {
+    Scanner scanner = new Scanner(System.in);
 
     @Test
     public void multiplication() {
+        int a = 0;
+        int b = 0;
+        int multiplication , answer;
 
+        for (int j = 1 ; j <= 2 ; j++) {
 
-        for(int i = 1 ; i<= 100 ; i++) {
+            multiplication = a * b;
 
-            int a = -100 + (int) (Math.random() * 100);
-            int b = -100 + (int) (Math.random() * 100);
+            answer = Warm_Up.multiplication(a,b);
 
-            int multiplication = a * b;
+            assertEquals("Тест провален", answer, multiplication);
 
-            int answer = Warm_Up.multiplication(a, b);
+            for (int i = 1; i <= 100; i++) {
 
-            assertEquals("Тест провален" , answer, multiplication);
+                a = -100 + (int) (Math.random() * 100);
+                b = -100 + (int) (Math.random() * 100);
+
+                multiplication = a * b;
+
+                answer = Warm_Up.multiplication(a, b);
+
+                assertEquals("Тест провален", answer, multiplication);
+            }
         }
     }
 }
